4379bdda21ef85b48d0d7d9c2d9330c6c1656260
Johan Rylander <johan@rylander.cc>
2022-12-04 15:39:12 +0100

    Initial

---
+++ home.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }:
+
+{
+  home = {
+    # Home Manager needs a bit of information about you and the
+    # paths it should manage.
+    username = "jrylander";
+    homeDirectory = "/home/jrylander";
+
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "22.11";
+    
+    packages = with pkgs; [
+      chromium signal-desktop
+    ];
+  };
+
+  programs = {
+    # Let Home Manager install and manage itself.
+    home-manager.enable = true;
+
+    tmux = {
+      enable = true;
+      shortcut = "w";
+      keyMode = "vi";
+      customPaneNavigationAndResize = true;
+    };
+
+    git = {
+      userEmail = "johan@rylander.cc";
+      userName = "Johan Rylander";
+    };
+  };
+}
