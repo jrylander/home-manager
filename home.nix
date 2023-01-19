ea5e5e2a25af68a8d3e3f7dbf6f05bb17d7e9864
Johan Rylander <johan@rylander.cc>
2023-01-19 18:44:29 +0100

    For headless machines

--- home.nix
+++ home.nix
@@ -4,30 +4,13 @@
   nixpkgs.config.allowUnfreePredicate = (_: true);
 
   home = {
-    # Home Manager needs a bit of information about you and the
-    # paths it should manage.
     username = "jrylander";
     homeDirectory = "/home/jrylander";
 
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
-    stateVersion = "22.11";
-
-
     packages = with pkgs; [
-      ansible
       bind
-      bitwarden-cli
-      chezmoi
       fd
       git
-      gnumake
       killall
     ];
 
@@ -104,4 +87,14 @@
       '';
     };
   };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  stateVersion = "22.11";
 }
